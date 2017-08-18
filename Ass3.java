34d31ec11425eb7ed7a75f7172ebd9fabba0bdb7
sdeepica <sdeeps1028@gmail.com>
2017-08-18 14:56:26 +0530

    Add files via upload

---
+++ Ass3.java
@@ -0,0 +1,19 @@
+package assignment;
+import java.util.TreeSet;
+public class Ass3 {
+	public static void main(String[] args) {
+		TreeSet<String>str=new TreeSet<String>();
+		str.add("dog");
+		str.add("doonkey");
+		str.add("rat");
+		str.add("rabbit");
+		str.add("elephant");
+		System.out.println(str);
+	
+				
+					
+		
+		
+	}
+
+}
