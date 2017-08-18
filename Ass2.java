a578ba90951aa36774586db5e2a77f61f7c28c10
sdeepica <sdeeps1028@gmail.com>
2017-08-18 14:17:07 +0530

    Add files via upload

---
+++ Ass2.java
@@ -0,0 +1,21 @@
+package assignment;
+
+public class Ass2 {
+	public static void main(String[] args) {
+		int a;
+		int b;
+		int x=25;
+		int y=13;
+		int product;
+		int result=x+y;
+		a=result/2;
+		b=x-a;
+		//System.out.println(a);
+		//System.out.println(b);
+		product=a*b;
+		System.out.println(product);
+		
+		
+	}
+
+}
